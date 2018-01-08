2e5c2d35bb3e2a73dd7c542ddf05b2d2f51435ec
Adeeb123456 <adeebpakistani@gmail.com>
2018-01-08 19:56:55 +0500

    asd

--- app/src/main/java/mysmartshare/com/smartsharemy/MainActivity.java
+++ app/src/main/java/mysmartshare/com/smartsharemy/MainActivity.java
@@ -1,5 +1,9 @@
 package mysmartshare.com.smartsharemy;
 
+import android.app.AppOpsManager;
+import android.content.Context;
+import android.content.Intent;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +13,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.KITKAT) {
+            if (!hasPermission()) {
+                startActivityForResult(
+                        new Intent(Settings.ACTION_USAGE_ACCESS_SETTINGS),
+                        900);
+            }
+        }
+    }
+
+
+    private boolean hasPermission() {
+        AppOpsManager appOps = (AppOpsManager)
+                getSystemService(Context.APP_OPS_SERVICE);
+        int mode = 0;
+        if (android.os.Build.VERSION.SDK_INT > android.os.Build.VERSION_CODES.KITKAT) {
+            mode = appOps.checkOpNoThrow(AppOpsManager.OPSTR_GET_USAGE_STATS,
+                    android.os.Process.myUid(), getPackageName());
+        }
+        return mode == AppOpsManager.MODE_ALLOWED;
     }
 }
